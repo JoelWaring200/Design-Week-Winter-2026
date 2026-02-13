d022436d5e099d1c19472dedb4bfd8638db76177
JoelWaring200 <000974820@mohawkcollege.ca>
2026-02-12 19:04:06 -0500

    even more stuff

---
+++ DW W26 Unity/Assets/Scripts/FactoryManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class FactoryManager : MonoBehaviour
+{
+    public static FactoryManager instance;
+
+    public bool FactoryHeadLeft = false;
+    public bool FactoryArmLeft = false;
+    public bool FactoryLegLeft = false;
+    public bool FactoryBodyRight = false;
+    public bool FactoryArmRight = false;
+    public bool FactoryLegRight = false;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        if (FactoryHeadLeft && FactoryArmLeft && FactoryLegLeft &&
+            FactoryBodyRight && FactoryArmRight && FactoryLegRight)
+        {
+            Debug.Log("YOU WIN");
+        }
+    }
+}
